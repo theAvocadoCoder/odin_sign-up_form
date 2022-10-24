3436ebb3db9b3f15f0f49326b1ba9d6c38d8e3a0
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-10-24 11:21:02 +0100

    Link script

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+function checkPasswordMatch() {
+  if (passwordInput.value !== confirmPasswordInput.value) {
+    displayErrorMessage();
+  } else {
+    removeErrorMessage();
+  }
+}
+
+function displayErrorMessage() {
+  const textNode = document.createTextNode("* Passwords do not match");
+  passwordInput.classList.toggle("error", true);
+  confirmPasswordInput.classList.toggle("error", true);
+  passwordErrorMessage.appendChild(textNode);
+}
+
+function removeErrorMessage() {
+  if (passwordErrorMessage.firstChild) {
+    passwordErrorMessage.removeChild(passwordErrorMessage.firstChild); 
+    passwordInput.classList.toggle("error", false);
+    confirmPasswordInput.classList.toggle("error", false);
+  }
+}
+
+
+const passwordErrorMessage = document.querySelector("#password-error-message"),
+  passwordInput = document.querySelector("#password"),
+  confirmPasswordInput = document.querySelector("#confirm-password"),
+  submitButton = document.querySelector("button");
+
+confirmPasswordInput.addEventListener("change", checkPasswordMatch);
+
+submitButton.addEventListener("click", (e)=>{
+  e.preventDefault;
+  checkPasswordMatch();
+})
